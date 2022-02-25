095263e962be31742bb13d690e9dd8d3cb8cfaa4
sssawant1 <87284685+sssawant1@users.noreply.github.com>
2022-02-25 18:03:17 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+//JSon format
+var student ={
+  name:"Holly",
+  class:7,
+  rolenumber:21,
+  marks:[94,96,98,92]
+};
+
+
+function setup() {
+  createCanvas(400, 400);
+  console.log(student.name);
+  console.log(student.class);
+  
+}
+
+function draw() {
+  background(220);
+}
